fc35e753eb7b7606e22ad586ee57cc6daa93c60e
github username <github email address>
2018-09-20 17:26:19 -0400

    made some shit

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash 
 
 #READ
   get '/songs' do
@@ -6,10 +9,33 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
+  get '/songs/new' do
+    erb :'songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     # binding.pry
     erb :'songs/show'
   end
+
+
+  post '/songs' do
+    @song = Song.create(name: params["song_name"])
+    @song.artist = Artist.find_or_create_by(name: params["artist_name"])
+    @song.genre_ids = params[:genres]
+    # binding.pry
+    @song.save
+    redirect "songs/#{@song.slug}"
+  end
+
+  # post '/songs' do
+  #   song_name = params["song_name"]
+  #   artist = Artist.find_or_create_by(name: params["artist_name"])
+  #   genre_ids = params[:genres]
+  #   @song = Song.create
+  # end
+
+
 
 end
