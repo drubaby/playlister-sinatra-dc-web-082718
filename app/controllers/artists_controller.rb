4a2bc3c0b29365c9b7d89ae7d7303973c96e91d1
drubaby <peryton6@gmail.com>
2018-09-20 15:34:19 -0400

    make show pages for all 3 classes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,5 +4,11 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
     erb :'artists/index'
   end
-  
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    # binding.pry
+    erb :'artists/show'
+  end
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,15 @@
 class SongsController < ApplicationController
 
+#READ
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    # binding.pry
+    erb :'songs/show'
   end
 
 end
